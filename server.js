d69e58076084bb786aae90ff2d1b665cacd5f9ea
Alcantn1 <nalcantara92@gmail.com>
2023-06-10 13:41:27 -0700

    server/config files

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const sequelize = require('./config/connection');
+const routes = require('./routes');
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up sessions and handlebars
+const sess = {
+  secret: 'mySecret',
+  cookie: {
+    // Session will automatically expire in 10 minutes
+    expires: 10 * 60 * 1000
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+// Set up handlebars as the template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Parse incoming JSON data
+app.use(express.json());
+// Parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Use the routes defined in the 'routes' folder
+app.use(routes);
+
+// Sync sequelize models and start the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
